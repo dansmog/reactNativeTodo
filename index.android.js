4abdb1253f51e3e8de14e4f09cd4472a20b83afd
dansmog <juwonanthony@gmail.com>
2017-09-28 19:23:09 +0100

    Add todo list item

--- index.android.js
+++ index.android.js
@@ -3,37 +3,64 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  FlatList
 } from 'react-native';
-import * as firebase from 'firebase';
-import ActionButton from './ActionButton';
-import ListItem from './ListITem';
+// import * as firebase from 'firebase';
+// import ActionButton from './ActionButton';
+// import ListItem from './ListITem';
 
 
-var config = {
-    apiKey: "AIzaSyCaaH4Y2qYQys5y6orVhymewrqKwvnGJvM",
-    authDomain: "rntodoapp-766cd.firebaseapp.com",
-    databaseURL: "https://rntodoapp-766cd.firebaseio.com",
-    projectId: "rntodoapp-766cd",
-    storageBucket: "rntodoapp-766cd.appspot.com",
-    messagingSenderId: "95727791919"
-  };
-  const firebaseApp = firebase.initializeApp(config);
+// var config = {
+//     apiKey: "AIzaSyCaaH4Y2qYQys5y6orVhymewrqKwvnGJvM",
+//     authDomain: "rntodoapp-766cd.firebaseapp.com",
+//     databaseURL: "https://rntodoapp-766cd.firebaseio.com",
+//     projectId: "rntodoapp-766cd",
+//     storageBucket: "rntodoapp-766cd.appspot.com",
+//     messagingSenderId: "95727791919"
+//   };
+//   const firebaseApp = firebase.initializeApp(config);
 
 
 export default class reactNativeTodo extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      todos: [
+        {
+          id: Date.now() + 1,
+          todo: "Create a basic todo app in react-native",
+        },
+        {
+          id: Date.now() + 2,
+          todo: "Create a basic todo app in react-native",
+        },
+        {
+          id: Date.now() + 3,
+          todo: "Create a basic todo app in react-native",
+        },
+      ]
+    }
   }
 
-  onpress(){
-    alert('wor');
-  }
+  keyExtractor = (item, index) => item.id;
+
   render() {
     return (
       <View style={styles.container}>
-        <ListItem />
-        <ActionButton onPress={this.onpress} title="save to todo"/>
+        {/*<StatusBar 
+        backgroundColor="#fff" 
+        barStyle="light-content"
+        />*/}
+        <FlatList 
+          data={this.state.todos}
+          renderItem={({item}) => 
+            <View style={{backgroundColor: '#fff', padding: 10, marginTop: 10}}>
+              <Text style={{color: '#000'}}>{item.todo}</Text>
+            </View>
+          }
+          keyExtractor={this.keyExtractor}
+        />
       </View>
     );
   }
@@ -41,7 +68,7 @@ export default class reactNativeTodo extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#1abc9c',
+    backgroundColor: '#f3f3f3',
     flex: 1,
     padding: 20
   }
